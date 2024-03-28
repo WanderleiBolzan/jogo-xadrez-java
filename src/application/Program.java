4a2b61dccc5320ec9a5e2ee66c6a58fe547b7465
wanderlei jose bolzan <wbolzanjp@gmail.com>
2024-03-28 16:41:09 -0300

    Criadas as Classe: Posicao e Program

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import boardgame.Posicao;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
+		
+	}
+
+}
