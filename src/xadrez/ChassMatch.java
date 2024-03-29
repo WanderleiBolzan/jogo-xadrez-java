4d0b81fe6a7eeca6e57f46e8d034089520d1fafc
wanderlei jose bolzan <wbolzanjp@gmail.com>
2024-03-29 10:50:44 -0300

    ChessException and ChessPosition

--- src/xadrez/ChassMatch.java
+++ src/xadrez/ChassMatch.java
@@ -1,7 +1,6 @@
 package xadrez;
 
 import boardgame.Board;
-import boardgame.Position;
 import xadrez.pieces.King;
 import xadrez.pieces.Rook;
 
@@ -24,9 +23,13 @@ public class ChassMatch {
 		return mat;
 	}
 
+	private void placeNewPiece(char column, int row, ChassPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+		
+	}
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.BRANCO), new Position(2, 1));
-		board.placePiece(new King(board, Color.PRETO), new Position(0, 4));		
-		board.placePiece(new King(board, Color.BRANCO), new Position(7, 4));		
+		placeNewPiece('b', 6, new Rook(board, Color.BRANCO));
+		placeNewPiece('e', 8, new King(board, Color.PRETO));		
+		placeNewPiece('e', 1, new King(board, Color.BRANCO));		
 	}
 }
