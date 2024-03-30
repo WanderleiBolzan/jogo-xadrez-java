2d79d3a7f2fbf89e1db4c75630074621e7ef0a58
wanderlei jose bolzan <wbolzanjp@gmail.com>
2024-03-30 12:51:39 -0300

    Imprimindo possíveis movimentos

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,6 +23,10 @@ public class Program {
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 				
+				boolean[][] possibileMovies = chassMatch.possibleMovies(source);
+				UI.clearScreen();
+				UI.printBoard(chassMatch.getPieces(), possibileMovies);
+				
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
--- src/application/UI.java
+++ src/application/UI.java
@@ -52,16 +52,31 @@ public class UI {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
-				printPiece(pieces[i][j]);
+				printPiece(pieces[i][j], false);
 			}
 			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
 	}
 
-	private static void printPiece(ChassPiece piece) {
+	public static void printBoard(ChassPiece[][] pieces, boolean[][] possibleMovies) {
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], possibleMovies[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g h");
+	}
+	
+	
+	private static void printPiece(ChassPiece piece, boolean backgound) {
+		if (backgound) {
+			System.out.print(ANSI_BLUE_BACKGROUND);
+		}
     	if (piece == null) {
-            System.out.print("-");
+            System.out.print("-" + ANSI_RESET);
         }
         else {
             if (piece.getCor() == Color.BRANCO) {
